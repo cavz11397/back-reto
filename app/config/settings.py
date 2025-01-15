9299786f448398ba5809ff63cf6939b3a3bc4a34
cralvazu <cristian.vasquez@sofka.com.co>
2025-01-15 14:46:57 -0500

    eliminar items y visualizar grafico

--- app/config/settings.py
+++ app/config/settings.py
@@ -4,13 +4,22 @@ import yaml
 # Ruta base del proyecto
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Ruta al archivo config.yaml
+# Determinar el entorno
+# ENV = os.getenv('ENV', 'local')
+
+# Ruta al archivo de configuración según el entorno
 CONFIG_PATH = os.path.join(BASE_DIR, "config", "config.yaml")
+# CONFIG_PATH = os.path.join(BASE_DIR, "config", f"config.{ENV}.yaml")
 
 def load_config(file_path: str):
     try:
         with open(file_path, "r") as file:
             return yaml.safe_load(file)
+        # env = os.getenv('ENV', 'local')  # Por defecto, usa 'local' si no se especifica el entorno
+        # config_file = f'config.{env}.yaml'
+        # with open(config_file, 'r') as file:
+        #     configuration = yaml.safe_load(file)
+        # return configuration
     except FileNotFoundError:
         raise FileNotFoundError(f"No se encontró el archivo de configuración: {file_path}")
     except Exception as e:
